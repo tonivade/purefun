618e0aab02ea241935b18c9c68154241d94b0910
Antonio Muñoz <antonio.munoz@rumbo.com>
2018-02-28 08:36:21 +0100

    added java9 module-info definitions

---
+++ src/main/java9/module-info.java
@@ -0,0 +1,7 @@
+module com.github.tonivade.zeromock.core {
+  requires jdk.httpserver;
+  requires com.google.code.gson;
+  requires com.github.tonivade.equalizer;
+  
+  exports com.github.tonivade.zeromock.core;
+}
