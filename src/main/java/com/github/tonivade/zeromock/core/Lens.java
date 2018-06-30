81e10746cd96e2fe069176207b1e9736450c15c2
Antonio Muñoz <antoniogmc@gmail.com>
2018-06-30 20:35:22 +0200

    added license to new files

--- src/main/java/com/github/tonivade/zeromock/core/Lens.java
+++ src/main/java/com/github/tonivade/zeromock/core/Lens.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2018, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
 package com.github.tonivade.zeromock.core;
 
 import static java.util.Objects.requireNonNull;
