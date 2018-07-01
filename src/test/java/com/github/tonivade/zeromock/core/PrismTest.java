2416f83c3c7935c554094c448109dfa80b56e720
Antonio Muñoz <antoniogmc@gmail.com>
2018-07-01 13:24:16 +0200

    improved prism tests

--- src/test/java/com/github/tonivade/zeromock/core/PrismTest.java
+++ src/test/java/com/github/tonivade/zeromock/core/PrismTest.java
@@ -23,13 +23,17 @@ public class PrismTest {
   public void prismTest() {
     Option<String> some = Option.some("a");
     Option<String> none = Option.none();
-
     assertAll(() -> assertEquals(some, someString.getOption(some)),
+              () -> assertEquals(none, someString.getOption(none)),
               () -> assertEquals(some, someString.reverseGet("a")),
               () -> assertEquals(Option.some("A"), someString.modify(String::toUpperCase).apply(some)),
+              () -> assertEquals(none, someString.modify(String::toUpperCase).apply(none)),
               () -> assertEquals(Option.some("A"), someString.modifyOption(String::toUpperCase).apply(some).flatten()),
+              () -> assertEquals(none, someString.modifyOption(String::toUpperCase).apply(none)),
               () -> assertEquals(Option.some("A"), someString.set("A").apply(some)),
+              () -> assertEquals(none, someString.set("A").apply(none)),
               () -> assertEquals(Option.some("A"), someString.setOption("A").apply(some).flatten()),
+              () -> assertEquals(none, someString.setOption("A").apply(none)),
               () -> assertEquals(Either.right("a"), someString.getOrModify(some)),
               () -> assertEquals(Either.left(none), someString.getOrModify(none))
         );
@@ -42,11 +46,16 @@ public class PrismTest {
     Option<String> none = Option.none();
 
     assertAll(() -> assertEquals(Option.some(5), someStringToInteger.getOption(five)),
+              () -> assertEquals(none, someStringToInteger.getOption(none)),
               () -> assertEquals(five, someStringToInteger.reverseGet(5)),
               () -> assertEquals(Option.some("6"), someStringToInteger.modify(i -> i + 1).apply(five)),
+              () -> assertEquals(none, someStringToInteger.modify(i -> i + 1).apply(none)),
               () -> assertEquals(Option.some("6"), someStringToInteger.modifyOption(i -> i + 1).apply(five).flatten()),
+              () -> assertEquals(none, someStringToInteger.modifyOption(i -> i + 1).apply(none)),
               () -> assertEquals(Option.some("4"), someStringToInteger.set(4).apply(five)),
+              () -> assertEquals(none, someStringToInteger.set(4).apply(none)),
               () -> assertEquals(Option.some("4"), someStringToInteger.setOption(4).apply(five).flatten()),
+              () -> assertEquals(none, someStringToInteger.setOption(4).apply(none).flatten()),
               () -> assertEquals(some, someStringToInteger.set(4).apply(some)),
               () -> assertEquals(Either.right(5), someStringToInteger.getOrModify(five)),
               () -> assertEquals(Either.left(none), someString.getOrModify(none))
