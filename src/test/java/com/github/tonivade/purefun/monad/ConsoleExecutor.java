4b16a55dd604f64fb63d39523e21bb2ca0972a2d
Antonio Muñoz <antoniogmc@gmail.com>
2018-08-08 20:56:04 +0200

    console unit test

---
+++ src/test/java/com/github/tonivade/purefun/monad/ConsoleExecutor.java
@@ -0,0 +1,37 @@
+package com.github.tonivade.purefun.monad;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+public class ConsoleExecutor {
+
+  private final StringBuilder input = new StringBuilder();
+  private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+  
+  public ConsoleExecutor read(String string) {
+    input.append(string).append('\n');
+    return this;
+  }
+  
+  public void verify(String expected) {
+    assertEquals(expected, new String(output.toByteArray(), UTF_8));
+  }
+  
+  public <T> T run(IO<T> program) {
+    InputStream savedInput = System.in;
+    PrintStream savedOutput = System.out;
+    try {
+      System.setIn(new ByteArrayInputStream(input.toString().getBytes(UTF_8)));
+      System.setOut(new PrintStream(output));
+      return program.unsafeRunSync();
+    } finally {
+      System.setIn(savedInput);
+      System.setOut(savedOutput);
+    }
+  }
+}
--- src/test/java/com/github/tonivade/purefun/monad/IOTest.java
+++ src/test/java/com/github/tonivade/purefun/monad/IOTest.java
@@ -4,14 +4,12 @@
  */
 package com.github.tonivade.purefun.monad;
 
-import static com.github.tonivade.purefun.data.Sequence.listOf;
 import static com.github.tonivade.purefun.monad.IO.Console.print;
 import static com.github.tonivade.purefun.monad.IO.Console.read;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.github.tonivade.purefun.Nothing;
@@ -25,16 +23,21 @@ public class IOTest {
     assertAll(
         () -> assertEquals("hola mundo", unit.unsafeRunSync()),
         () -> assertEquals("HOLA MUNDO", unit.map(String::toUpperCase).unsafeRunSync()),
-        () -> assertArrayEquals(new String[]{"hola", "mundo"}, unit.flatMap(string -> IO.of(() -> string.split(" "))).unsafeRunSync()),
+        () -> assertArrayEquals(new String[] { "hola", "mundo" },
+            unit.flatMap(string -> IO.of(() -> string.split(" "))).unsafeRunSync()),
         () -> assertEquals(Integer.valueOf(100), unit.andThen(IO.of(() -> 100)).unsafeRunSync())
         );
   }
   
   @Test
-  @Disabled
   public void echo() {
-    IO<Nothing> echo = read().flatMap(message -> print("Hello " + message));
-    
-    IO.sequence(listOf(print("write your name"), echo, print("end"))).unsafeRunSync();
+    IO<Nothing> echo = print("write your name")
+      .andThen(read())
+      .flatMap(name -> print("Hello " + name))
+      .andThen(print("end"));
+
+    ConsoleExecutor executor = new ConsoleExecutor().read("Toni");
+    executor.run(echo);
+    executor.verify("write your name\nHello Toni\nend\n");
   }
 }
