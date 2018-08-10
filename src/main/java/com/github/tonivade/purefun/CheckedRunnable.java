8fbb0cad8a644ce08aec9dfabb45a55ab66cd657
Antonio Muñoz <antoniogmc@gmail.com>
2018-08-10 20:59:49 +0200

    CheckedRunnable added

---
+++ src/main/java/com/github/tonivade/purefun/CheckedRunnable.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
+package com.github.tonivade.purefun;
+
+@FunctionalInterface
+public interface CheckedRunnable {
+
+  void run() throws Exception;
+
+}
