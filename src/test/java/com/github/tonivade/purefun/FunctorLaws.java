e80aca168e11fac94fef54c3bdbc2c6fb92325ea
Antonio Muñoz <antoniogmc@gmail.com>
2018-09-02 20:12:44 +0200

    fixed problem with generic types in FunctorLaws and MonadLaws

--- src/test/java/com/github/tonivade/purefun/FunctorLaws.java
+++ src/test/java/com/github/tonivade/purefun/FunctorLaws.java
@@ -12,23 +12,23 @@ public class FunctorLaws {
   private final static Function1<String, String> toUpperCase = String::toUpperCase;
   private final static Function1<String, String> toLowerCase = String::toLowerCase;
 
-  public static <W extends Witness> void verifyLaws(Functor<W, String> functor) {
+  public static <W extends Witness, F extends Functor<W, String>> void verifyLaws(F functor) {
     assertAll(() -> identity(functor),
               () -> composition(functor),
               () -> associativity(functor));
   }
 
-  private static <W extends Witness> void identity(Functor<W, String> functor) {
+  private static <W extends Witness, F extends Functor<W, String>> void identity(F functor) {
     assertEquals(functor, functor.map(Function1.identity()), "identity law");
   }
 
-  private static <W extends Witness> void composition(Functor<W, String> functor) {
+  private static <W extends Witness, F extends Functor<W, String>> void composition(F functor) {
     assertEquals(functor.map(toUpperCase).map(toLowerCase),
                  functor.map(toUpperCase.andThen(toLowerCase)),
                  "composition law");
   }
 
-  private static <W extends Witness> void associativity(Functor<W, String> functor) {
+  private static <W extends Witness, F extends Functor<W, String>> void associativity(F functor) {
     assertEquals(functor.map(toUpperCase).map(toLowerCase.andThen(toUpperCase)),
                  functor.map(toUpperCase.andThen(toLowerCase)).map(toUpperCase),
                  "associativity law");
