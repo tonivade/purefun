78bc1e5ff9f5b5ff397d58d10477b7a21f3ce8cc
Antonio Muñoz <antoniogmc@gmail.com>
2019-05-26 22:54:53 +0200

    ref unit tests fixed

--- zio/src/test/java/com/github/tonivade/purefun/zio/RefTest.java
+++ zio/src/test/java/com/github/tonivade/purefun/zio/RefTest.java
@@ -9,6 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.github.tonivade.purefun.Nothing;
+import com.github.tonivade.purefun.Producer;
 import com.github.tonivade.purefun.type.Either;
 
 public class RefTest {
@@ -26,7 +28,9 @@ public class RefTest {
   public void set() {
     Ref<String> ref = Ref.of("Hello World!");
 
-    ZIO<Object, Object, String> result = ref.set("Something else").andThen(ref::get);
+    ZIO<Nothing, Void, Nothing> set = ref.set("Something else");
+    Producer<ZIO<Nothing, Void, String>> get = ref::get;
+    ZIO<Nothing, Void, String> result = set.andThen(get);
 
     assertEquals(Either.right("Something else"), result.provide(nothing()));
   }
@@ -35,7 +39,9 @@ public class RefTest {
   public void lazySet() {
     Ref<String> ref = Ref.of("Hello World!");
 
-    ZIO<Object, Object, String> result = ref.lazySet("Something else").andThen(ref::get);
+    ZIO<Nothing, Void, Nothing> lazySet = ref.lazySet("Something else");
+    Producer<ZIO<Nothing, Void, String>> get = ref::get;
+    ZIO<Nothing, Void, String> result = lazySet.andThen(get);
 
     assertEquals(Either.right("Something else"), result.provide(nothing()));
   }
@@ -45,7 +51,8 @@ public class RefTest {
     Ref<String> ref = Ref.of("Hello World!");
 
     ZIO<Object, Object, String> result = ref.getAndSet("Something else");
-    ZIO<Object, Object, String> afterUpdate = ref.get();
+    Producer<ZIO<Object, Object, String>> get = ref::get;
+    ZIO<Object, Object, String> afterUpdate = result.andThen(get);
 
     assertEquals(Either.right("Hello World!"), result.provide(nothing()));
     assertEquals(Either.right("Something else"), afterUpdate.provide(nothing()));
@@ -56,7 +63,8 @@ public class RefTest {
     Ref<String> ref = Ref.of("Hello World!");
 
     ZIO<Object, Object, String> result = ref.getAndUpdate(String::toUpperCase);
-    ZIO<Object, Object, String> afterUpdate = ref.get();
+    Producer<ZIO<Object, Object, String>> get = ref::get;
+    ZIO<Object, Object, String> afterUpdate = result.andThen(get);
 
     assertEquals(Either.right("Hello World!"), result.provide(nothing()));
     assertEquals(Either.right("HELLO WORLD!"), afterUpdate.provide(nothing()));
