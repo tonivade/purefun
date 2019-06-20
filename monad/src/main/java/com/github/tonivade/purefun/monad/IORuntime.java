a25d261859ae7e92cd47798ca4eb1dd691ad9711
Antonio Muñoz <antoniogmc@gmail.com>
2019-06-20 23:02:37 +0200

    missing license header in new files

--- monad/src/main/java/com/github/tonivade/purefun/monad/IORuntime.java
+++ monad/src/main/java/com/github/tonivade/purefun/monad/IORuntime.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2018-2019, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
 package com.github.tonivade.purefun.monad;
 
 import com.github.tonivade.purefun.Higher1;
