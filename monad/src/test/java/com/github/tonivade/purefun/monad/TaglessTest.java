ed2519aba6b1fc4411bcc3551ed7ad0401b481ee
Antonio Muñoz <antoniogmc@gmail.com>
2019-06-29 18:58:23 +0200

    minor refactor

--- monad/src/test/java/com/github/tonivade/purefun/monad/TaglessTest.java
+++ monad/src/test/java/com/github/tonivade/purefun/monad/TaglessTest.java
@@ -10,7 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import com.github.tonivade.purefun.Function1;
 import com.github.tonivade.purefun.Higher1;
 import com.github.tonivade.purefun.Kind;
 import com.github.tonivade.purefun.Tuple2;
@@ -18,17 +17,19 @@ import com.github.tonivade.purefun.Unit;
 import com.github.tonivade.purefun.data.ImmutableList;
 import com.github.tonivade.purefun.instances.IOInstances;
 import com.github.tonivade.purefun.instances.StateInstances;
+import com.github.tonivade.purefun.typeclasses.For;
 import com.github.tonivade.purefun.typeclasses.Monad;
 
 public class TaglessTest {
 
-  final Program<Higher1<State.µ, ImmutableList<String>>, StateProgramInterpreter> stateProgram =
-      new Program<>(new StateProgramInterpreter());
-  final Program<IO.µ, IOProgramInterpreter> ioProgram = new Program<>(new IOProgramInterpreter());
+  final Program<Higher1<State.µ, ImmutableList<String>>> stateProgram =
+      new Program<>(StateInstances.monad(), new StateProgramInterpreter());
+  final Program<IO.µ> ioProgram =
+      new Program<>(IOInstances.monad(), new IOProgramInterpreter());
 
   @Test
   public void stateInterpreter() {
-    State<ImmutableList<String>, Unit> state = State.narrowK(stateProgram.echo());
+    State<ImmutableList<String>, Unit> state = stateProgram.echo().fix1(State::narrowK);
 
     Tuple2<ImmutableList<String>, Unit> run = state.run(listOf("Toni"));
 
@@ -39,92 +40,77 @@ public class TaglessTest {
   public void ioInterpreter() {
     ConsoleExecutor executor = new ConsoleExecutor().read("Toni");
 
-    executor.run(IO.narrowK(ioProgram.echo()));
+    executor.run(ioProgram.echo().fix1(IO::narrowK));
 
     assertEquals("what's your name?\nHello Toni\n", executor.getOutput());
   }
 }
 
-interface IOProgramT<F extends Kind> {
+interface ProgramK<F extends Kind> {
 
   Higher1<F, String> read();
 
   Higher1<F, Unit> write(String string);
 }
 
-class Program<F extends Kind, P extends IOProgramT<F> & Monad<F>> {
+class Program<F extends Kind> {
 
-  final P program;
+  final Monad<F> monad;
+  final ProgramK<F> io;
 
-  Program(P program) {
-    this.program = requireNonNull(program);
+  Program(Monad<F> monad, ProgramK<F> io) {
+    this.monad = requireNonNull(monad);
+    this.io = requireNonNull(io);
   }
 
   Higher1<F, Unit> echo() {
-    return program.flatMap(readName(), this::sayHello);
+    return For.with(monad)
+        .andThen(this::whatsYourName)
+        .andThen(this::readName)
+        .flatMap(this::sayHello)
+        .get();
+  }
+
+  private Higher1<F, Unit> whatsYourName() {
+    return io.write("what's your name?");
   }
 
   private Higher1<F, String> readName() {
-    return program.flatMap(program.write("what's your name?"), nothing -> program.read());
+    return io.read();
   }
 
   private Higher1<F, Unit> sayHello(String name) {
-    return program.write("Hello " + name);
+    return io.write("Hello " + name);
   }
 }
 
-class IOProgramInterpreter implements IOProgramT<IO.µ>, Monad<IO.µ> {
+class IOProgramInterpreter implements ProgramK<IO.µ> {
 
-  final Monad<IO.µ> monad = IOInstances.monad();
   final Console<IO.µ> console = Console.io();
 
   @Override
-  public <T> IO<T> pure(T value) {
-    return IO.narrowK(monad.pure(value));
-  }
-
-  @Override
-  public <T, R> IO<R> flatMap(Higher1<IO.µ, T> value, Function1<T, ? extends Higher1<IO.µ, R>> map) {
-    return IO.narrowK(monad.flatMap(value, map));
-  }
-
-  @Override
   public IO<String> read() {
-    return IO.narrowK(console.readln());
+    return console.readln().fix1(IO::narrowK);
   }
 
   @Override
   public IO<Unit> write(String string) {
-    return IO.narrowK(console.println(string));
+    return console.println(string).fix1(IO::narrowK);
   }
 }
 
 class StateProgramInterpreter
-    implements IOProgramT<Higher1<State.µ, ImmutableList<String>>>,
-               Monad<Higher1<State.µ, ImmutableList<String>>> {
+    implements ProgramK<Higher1<State.µ, ImmutableList<String>>> {
 
-  final Monad<Higher1<State.µ, ImmutableList<String>>> monad = StateInstances.monad();
   final Console<Higher1<State.µ, ImmutableList<String>>> console = Console.state();
 
   @Override
-  public <T> State<ImmutableList<String>, T> pure(T value) {
-    return State.narrowK(monad.pure(value));
-  }
-
-  @Override
-  public <T, R> State<ImmutableList<String>, R> flatMap(
-      Higher1<Higher1<State.µ, ImmutableList<String>>, T> value,
-      Function1<T, ? extends Higher1<Higher1<State.µ, ImmutableList<String>>, R>> map) {
-    return State.narrowK(monad.flatMap(value, map));
-  }
-
-  @Override
   public State<ImmutableList<String>, String> read() {
-    return State.narrowK(console.readln());
+    return console.readln().fix1(State::narrowK);
   }
 
   @Override
   public State<ImmutableList<String>, Unit> write(String string) {
-    return State.narrowK(console.println(string));
+    return console.println(string).fix1(State::narrowK);
   }
 }
