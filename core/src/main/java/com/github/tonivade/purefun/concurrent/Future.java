4de6fffba885e4119b3938f0b234f1d5adcdbab4
Antonio Muñoz <antoniogmc@gmail.com>
2019-11-26 19:17:01 +0100

    some code clean up and documentation

--- core/src/main/java/com/github/tonivade/purefun/concurrent/Future.java
+++ core/src/main/java/com/github/tonivade/purefun/concurrent/Future.java
@@ -24,6 +24,39 @@ import com.github.tonivade.purefun.Producer;
 import com.github.tonivade.purefun.Unit;
 import com.github.tonivade.purefun.type.Try;
 
+/**
+ * <p>This type is an abstraction of a computation deferred in time, that will be eventually executed in the future to obtain
+ * a result. Despite other implementations of Future, this implementation doesn't execute immediately the computation in another
+ * thread. Only when the method {@code apply} is called, providing an executor or using the default executor, the computation is started.
+ * Then, the result of calling {@code apply} method, is a {@code Promise}. The user of the library can subscribe to events of the promise like
+ * {@code onComplete}, {@code onSuccess} or {@code onFailure}; or call to {@code get} method and wait to obtain the result of the computation.</p>
+ *
+ * <p>The result of the computation is a {@code Try<T>}, this means that the computation can end successfully or with an error.</p>
+ *
+ * <p>You can create instances of {@code Future} in this way:</p>
+ * <ul>
+ *   <li>Future.success(value): returns a future that returns successfully with the given value.</li>
+ *   <li>Future.failure(error): returns a future that returns an error with the given error.</li>
+ *   <li>Future.run(computation): returns a future that eventually will execute the given computation.</li>
+ *   <li>Future.exec(runnable): returns a future that eventually will execute the given runnable.</li>
+ *   <li>Future.delay(duration, computation): returns a future that eventually will execute the given computation, but after waiting the given duration.</li>
+ *   <li>Future.defer(computation): returns a future that eventually will execute the given computation that returns another Future.</li>
+ *   <li>Future.bracket(acquire, usage, release): returns a future that eventually will acquire a resource, then use it, and finally release it.</li>
+ * </ul>
+ *
+ * <p>A future can be cancelable by calling the method {@code cancel}. If the future has not been executed yet, the future will be cancelled
+ * and the result of the computation will be a {@code Try.failure(CancellableException)}, but if the future has been executed, and is completed
+ * the calling of cancel method will not have any consequences. If the computation is running when the cancel method is called, and if the flag
+ * mayInterruptThread is true, then it will try to interrupt the thread running the computation and the result of the computation
+ * will be also a {@code Try.failure(CancellableException)}.</p>
+ *
+ * <p>If during the execution of the computation in a thread, this thread is interrupted for any reason, the result
+ * of the computation will be a {@code Try.failure(InterruptedException)}.</p>
+ *
+ * @param <T> result of the computation
+ * @see Try
+ * @see Promise
+ */
 @HigherKind
 public interface Future<T> {
 
@@ -97,6 +130,10 @@ public interface Future<T> {
 
   static <T> Future<T> defer(Producer<Future<T>> producer) {
     return run(producer::get).flatMap(identity());
+  }
+
+  static <T extends AutoCloseable, R> Future<R> bracket(Future<T> acquire, Function1<T, Future<R>> use) {
+    return FutureImpl.bracket(acquire, use, AutoCloseable::close);
   }
 
   static <T, R> Future<R> bracket(Future<T> acquire, Function1<T, Future<R>> use, Consumer1<T> release) {
@@ -179,27 +216,24 @@ final class FutureImpl<T> implements Future<T> {
 
   protected static <T, R> Future<R> transform(Future<T> current, Function1<Try<T>, Try<R>> mapper) {
     return new FutureImpl<>(
-        (executor, promise, cancel) -> {
-          current.apply(executor).onComplete(value -> promise.tryComplete(mapper.apply(value)));
-        });
+        (executor, promise, cancellable) ->
+          current.apply(executor).onComplete(value -> promise.tryComplete(mapper.apply(value))));
   }
 
   protected static <T, R> Future<R> chain(Future<T> current, Function1<Try<T>, Future<R>> mapper) {
     return new FutureImpl<>(
-        (executor, promise, cancel) -> {
+        (executor, promise, cancellable) ->
           current.apply(executor).onComplete(
-            value -> mapper.apply(value).apply(executor).onComplete(promise::tryComplete));
-        });
+            value -> mapper.apply(value).apply(executor).onComplete(promise::tryComplete)));
   }
 
   protected static <T> Future<T> async(Producer<Try<T>> producer) {
     return new FutureImpl<>(
-        (executor, promise, cancel) -> {
+        (executor, promise, cancellable) ->
           executor.execute(() -> {
-            cancel.updateThread();
+            cancellable.updateThread();
             promise.tryComplete(producer.get());
-          });
-        });
+          }));
   }
 
   protected static <T> Future<T> from(Promise<T> promise) {
@@ -208,11 +242,10 @@ final class FutureImpl<T> implements Future<T> {
 
   protected static <T, R> Future<R> bracket(Future<T> acquire, Function1<T, Future<R>> use, Consumer1<T> release) {
     return new FutureImpl<>(
-      (executor, promise, cancellable) -> {
+      (executor, promise, cancellable) ->
         acquire.apply(executor).onComplete(
           resource -> resource.fold(Future::failure, use).apply(executor)
             .onComplete(promise::tryComplete)
-            .onComplete(result -> resource.onSuccess(release)));
-      });
+            .onComplete(result -> resource.onSuccess(release))));
   }
 }
