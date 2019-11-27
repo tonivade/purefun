7c9b2088aefd29dcd585fc9c0bf94004fba35ff4
Antonio Muñoz <antoniogmc@gmail.com>
2019-11-27 11:37:25 +0100

    unused import removed

--- core/src/test/java/com/github/tonivade/purefun/concurrent/FutureTest.java
+++ core/src/test/java/com/github/tonivade/purefun/concurrent/FutureTest.java
@@ -22,7 +22,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutorService;
