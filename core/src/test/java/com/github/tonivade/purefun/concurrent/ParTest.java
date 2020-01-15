77d560e6a7e58d6bdb124972e66d4f4715615b55
Antonio Muñoz <antoniogmc@gmail.com>
2020-01-15 23:04:09 +0100

    fixed duplications

--- core/src/test/java/com/github/tonivade/purefun/concurrent/ParTest.java
+++ core/src/test/java/com/github/tonivade/purefun/concurrent/ParTest.java
@@ -13,8 +13,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
@@ -23,45 +21,14 @@ import static org.mockito.Mockito.verify;
 public class ParTest {
 
   @Test
-  public void onCompleteSuccess(@Mock Consumer1<Try<String>> tryConsumer) {
+  public void onSuccess(@Mock Consumer1<String> consumerSuccess) {
     Par<String> par = Par.success("Hello World!");
 
-    Future<String> future = par.apply(Future.DEFAULT_EXECUTOR);
-    future.onComplete(tryConsumer).await();
-
-    assertAll(
-        () -> verify(tryConsumer, timeout(1000)).accept(Try.success("Hello World!")),
-        () -> assertTrue(future::isCompleted),
-        () -> assertFalse(future::isCancelled),
-        () -> assertEquals(Try.success("Hello World!"), future.await()));
-  }
-
-  @Test
-  public void onCompleteFailure(@Mock Consumer1<Try<String>> tryConsumer) {
-    UnsupportedOperationException error = new UnsupportedOperationException();
-    Par<String> par = Par.failure(error);
-
-    Future<String> future = par.apply(Future.DEFAULT_EXECUTOR);
-    future.onComplete(tryConsumer).await();
-
-    assertAll(
-        () -> verify(tryConsumer, timeout(1000)).accept(any()),
-        () -> assertTrue(future::isCompleted),
-        () -> assertFalse(future::isCancelled),
-        () -> assertEquals(Try.failure(error), future.await()));
-  }
-
-  @Test
-  public void onSuccess(@Mock Consumer1<String> consumerSuccess) {
-    Future<String> future = Future.success("Hello World!");
-
-    future.onSuccess(consumerSuccess).await();
+    Try<String> result = par.apply(Future.DEFAULT_EXECUTOR).onSuccess(consumerSuccess).await();
 
     assertAll(
         () -> verify(consumerSuccess, timeout(1000)).accept("Hello World!"),
-        () -> assertTrue(future::isCompleted),
-        () -> assertFalse(future::isCancelled),
-        () -> assertEquals(Try.success("Hello World!"), future.await()));
+        () -> assertEquals(Try.success("Hello World!"), result));
   }
 
   @Test
@@ -69,14 +36,11 @@ public class ParTest {
     UnsupportedOperationException error = new UnsupportedOperationException();
     Par<String> par = Par.failure(error);
 
-    Future<String> future = par.apply(Future.DEFAULT_EXECUTOR);
-    future.onFailure(consumerFailure);
+    Try<String> result = par.apply(Future.DEFAULT_EXECUTOR).onFailure(consumerFailure).await();
 
     assertAll(
         () -> verify(consumerFailure, timeout(1000)).accept(any()),
-        () -> assertTrue(future::isCompleted),
-        () -> assertFalse(future::isCancelled),
-        () -> assertEquals(Try.failure(error), future.await()));
+        () -> assertEquals(Try.failure(error), result));
   }
 
   @Test
