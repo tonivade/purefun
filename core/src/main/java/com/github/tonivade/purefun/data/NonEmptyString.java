97c13d9cdc0774dcf12a8c70d06ed726bf617bf9
Antonio Muñoz <antoniogmc@gmail.com>
2020-01-19 20:10:30 +0100

    added data type NonEmptyString

---
+++ core/src/main/java/com/github/tonivade/purefun/data/NonEmptyString.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2018-2020, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
+package com.github.tonivade.purefun.data;
+
+import com.github.tonivade.purefun.Equal;
+import com.github.tonivade.purefun.Function1;
+import com.github.tonivade.purefun.Operator1;
+
+import java.util.Objects;
+
+import static com.github.tonivade.purefun.type.Validation.requireNonEmpty;
+
+public class NonEmptyString {
+
+  private static final Equal<NonEmptyString> EQUAL = Equal.<NonEmptyString>of().comparing(x -> x.value);
+
+  private final String value;
+
+  private NonEmptyString(String value) {
+    this.value = value;
+  }
+
+  public NonEmptyString map(Operator1<String> mapper) {
+    return NonEmptyString.of(mapper.apply(value));
+  }
+
+  public <T> T transform(Function1<String, T> mapper) {
+    return mapper.apply(value);
+  }
+
+  public String get() {
+    return value;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return EQUAL.applyTo(this, obj);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(value);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("NonEmptyString(%s)", value);
+  }
+
+  public static NonEmptyString of(String value) {
+    return requireNonEmpty(value).map(NonEmptyString::new).getOrElseThrow();
+  }
+}
---
+++ core/src/test/java/com/github/tonivade/purefun/data/NonEmptyStringTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2018-2020, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
+package com.github.tonivade.purefun.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class NonEmptyStringTest {
+
+  @Test
+  public void nonEmptyString() {
+    NonEmptyString nonEmptyString = NonEmptyString.of("hola mundo");
+
+    assertAll(
+        () -> assertThrows(IllegalArgumentException.class, () -> NonEmptyString.of(null)),
+        () -> assertThrows(IllegalArgumentException.class, () -> NonEmptyString.of("")),
+        () -> assertDoesNotThrow(() -> NonEmptyString.of("hola mundo")),
+        () -> assertEquals("hola mundo", nonEmptyString.get()),
+        () -> assertEquals("HOLA MUNDO", nonEmptyString.transform(String::toUpperCase)),
+        () -> assertEquals(NonEmptyString.of("HOLA MUNDO"), nonEmptyString.map(String::toUpperCase)),
+        () -> assertEquals(NonEmptyString.of("hola mundo"), NonEmptyString.of("hola mundo"))
+    );
+  }
+}
