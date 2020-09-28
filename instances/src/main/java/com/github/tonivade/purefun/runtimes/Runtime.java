d82d45a5c6e4bb8da2df000c4226b4de6b52e9c7
Antonio Muñoz <antoniogmc@gmail.com>
2020-09-28 22:37:12 +0200

    minor refactor

--- instances/src/main/java/com/github/tonivade/purefun/runtimes/Runtime.java
+++ instances/src/main/java/com/github/tonivade/purefun/runtimes/Runtime.java
@@ -28,7 +28,7 @@ import com.github.tonivade.purefun.effect.ZIO_;
 import com.github.tonivade.purefun.monad.IO_;
 
 public interface Runtime<F extends Witness> {
-  
+
   <T> T run(Kind<F, T> value);
   
   <T> Future<T> parRun(Kind<F, T> value, Executor executor);
@@ -38,100 +38,149 @@ public interface Runtime<F extends Witness> {
   }
   
   static Runtime<IO_> io() {
-    return new Runtime<IO_>() {
-      @Override
-      public <T> T run(Kind<IO_, T> value) {
-        return value.fix(toIO()).unsafeRunSync();
-      }
-      
-      @Override
-      public <T> Future<T> parRun(Kind<IO_, T> value, Executor executor) {
-        return value.fix(toIO()).foldMap(async()).fix(toFuture());
-      }
-    };
+    return IORuntime.INSTANCE;
   }
   
   static Runtime<UIO_> uio() {
-    return new Runtime<UIO_>() {
-      @Override
-      public <T> T run(Kind<UIO_, T> value) {
-        return value.fix(toUIO()).unsafeRunSync();
-      }
-      
-      @Override
-      public <T> Future<T> parRun(Kind<UIO_, T> value, Executor executor) {
-        return value.fix(toUIO()).foldMap(async()).fix(toFuture());
-      }
-    };
+    return UIORuntime.INSTANCE;
   }
   
   static Runtime<Task_> task() {
-    return new Runtime<Task_>() {
-      @Override
-      public <T> T run(Kind<Task_, T> value) {
-        return value.fix(toTask()).safeRunSync().getOrElseThrow();
-      }
-      
-      @Override
-      public <T> Future<T> parRun(Kind<Task_, T> value, Executor executor) {
-        return value.fix(toTask()).foldMap(async()).fix(toFuture());
-      }
-    };
+    return TaskRuntime.INSTANCE;
   }
   
   static <R> Runtime<Kind<URIO_, R>> urio(R env) {
-    return new Runtime<Kind<URIO_, R>>() {
-      @Override
-      public <T> T run(Kind<Kind<URIO_, R>, T> value) {
-        return value.fix(toURIO()).unsafeRunSync(env);
-      }
-      
-      @Override
-      public <T> Future<T> parRun(Kind<Kind<URIO_, R>, T> value, Executor executor) {
-        return value.fix(toURIO()).foldMap(env, async()).fix(toFuture());
-      }
-    };
+    return URIORuntime.instance(env);
+  }
+  
+  @SuppressWarnings("unchecked")
+  static <E> Runtime<Kind<EIO_, E>> eio() {
+    return EIORuntime.INSTANCE;
   }
   
   static <R> Runtime<Kind<RIO_, R>> rio(R env) {
-    return new Runtime<Kind<RIO_, R>>() {
-      @Override
-      public <T> T run(Kind<Kind<RIO_, R>, T> value) {
-        return value.fix(toRIO()).safeRunSync(env).getOrElseThrow();
-      }
-      
-      @Override
-      public <T> Future<T> parRun(Kind<Kind<RIO_, R>, T> value, Executor executor) {
-        return value.fix(toRIO()).foldMap(env, async()).fix(toFuture());
-      }
-    };
-  }
-  
-  static <E> Runtime<Kind<EIO_, E>> eio() {
-    return new Runtime<Kind<EIO_, E>>() {
-      @Override
-      public <T> T run(Kind<Kind<EIO_, E>, T> value) {
-        return value.fix(toEIO()).safeRunSync().getRight();
-      }
-      
-      @Override
-      public <T> Future<T> parRun(Kind<Kind<EIO_, E>, T> value, Executor executor) {
-        return value.fix(toEIO()).foldMap(async()).fix(toFuture());
-      }
-    };
+    return RIORuntime.instance(env);
   }
   
   static <R, E> Runtime<Kind<Kind<ZIO_, R>, E>> zio(R env) {
-    return new Runtime<Kind<Kind<ZIO_, R>, E>>() {
-      @Override
-      public <T> T run(Kind<Kind<Kind<ZIO_, R>, E>, T> value) {
-        return value.fix(toZIO()).provide(env).getRight();
-      }
-      
-      @Override
-      public <T> Future<T> parRun(Kind<Kind<Kind<ZIO_, R>, E>, T> value, Executor executor) {
-        return value.fix(toZIO()).foldMap(env, async()).fix(toFuture());
-      }
-    };
+    return ZIORuntime.instance(env);
+  }
+}
+
+interface IORuntime extends Runtime<IO_> {
+  
+  IORuntime INSTANCE = new IORuntime() {};
+
+  @Override
+  default <T> T run(Kind<IO_, T> value) {
+    return value.fix(toIO()).unsafeRunSync();
+  }
+
+  @Override
+  default <T> Future<T> parRun(Kind<IO_, T> value, Executor executor) {
+    return value.fix(toIO()).foldMap(async()).fix(toFuture());
+  }
+}
+
+interface UIORuntime extends Runtime<UIO_> {
+  
+  UIORuntime INSTANCE = new UIORuntime() {};
+
+  @Override
+  default <T> T run(Kind<UIO_, T> value) {
+    return value.fix(toUIO()).unsafeRunSync();
+  }
+
+  @Override
+  default <T> Future<T> parRun(Kind<UIO_, T> value, Executor executor) {
+    return value.fix(toUIO()).foldMap(async()).fix(toFuture());
+  }
+}
+
+interface TaskRuntime extends Runtime<Task_> {
+  
+  TaskRuntime INSTANCE = new TaskRuntime() {};
+
+  @Override
+  default <T> T run(Kind<Task_, T> value) {
+    return value.fix(toTask()).safeRunSync().getOrElseThrow();
+  }
+
+  @Override
+  default <T> Future<T> parRun(Kind<Task_, T> value, Executor executor) {
+    return value.fix(toTask()).foldMap(async()).fix(toFuture());
+  }
+}
+
+interface EIORuntime<E> extends Runtime<Kind<EIO_, E>> {
+  
+  @SuppressWarnings("rawtypes")
+  EIORuntime INSTANCE = new EIORuntime() {};
+
+  @Override
+  default <T> T run(Kind<Kind<EIO_, E>, T> value) {
+    return value.fix(toEIO()).safeRunSync().getRight();
+  }
+
+  @Override
+  default <T> Future<T> parRun(Kind<Kind<EIO_, E>, T> value, Executor executor) {
+    return value.fix(toEIO()).foldMap(async()).fix(toFuture());
+  }
+}
+
+interface URIORuntime<R> extends Runtime<Kind<URIO_, R>> {
+  
+  static <R> URIORuntime<R> instance(R env) {
+    return () -> env;
+  }
+
+  R env();
+
+  @Override
+  default <T> T run(Kind<Kind<URIO_, R>, T> value) {
+    return value.fix(toURIO()).safeRunSync(env()).getOrElseThrow();
+  }
+
+  @Override
+  default <T> Future<T> parRun(Kind<Kind<URIO_, R>, T> value, Executor executor) {
+    return value.fix(toURIO()).foldMap(env(), async()).fix(toFuture());
+  }
+}
+
+interface RIORuntime<R> extends  Runtime<Kind<RIO_, R>> {
+  
+  static <R> RIORuntime<R> instance(R env) {
+    return () -> env;
+  }
+
+  R env();
+
+  @Override
+  default <T> T run(Kind<Kind<RIO_, R>, T> value) {
+    return value.fix(toRIO()).safeRunSync(env()).getOrElseThrow();
+  }
+
+  @Override
+  default <T> Future<T> parRun(Kind<Kind<RIO_, R>, T> value, Executor executor) {
+    return value.fix(toRIO()).foldMap(env(), async()).fix(toFuture());
+  }
+}
+
+interface ZIORuntime<R, E> extends  Runtime<Kind<Kind<ZIO_, R>, E>> {
+  
+  static <R, E> ZIORuntime<R, E> instance(R env) {
+    return () -> env;
+  }
+
+  R env();
+
+  @Override
+  default <T> T run(Kind<Kind<Kind<ZIO_, R>, E>, T> value) {
+    return value.fix(toZIO()).provide(env()).getRight();
+  }
+
+  @Override
+  default <T> Future<T> parRun(Kind<Kind<Kind<ZIO_, R>, E>, T> value, Executor executor) {
+    return value.fix(toZIO()).foldMap(env(), async()).fix(toFuture());
   }
 }
