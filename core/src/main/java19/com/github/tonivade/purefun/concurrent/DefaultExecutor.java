3c3be55080e390ad1d53fbb4bb1e02d798f2732b
Antonio Muñoz <antoniogmc@gmail.com>
2022-11-09 22:28:12 +0100

    use virtual threads as default executor in java 19

---
+++ core/src/main/java19/com/github/tonivade/purefun/concurrent/DefaultExecutor.java
@@ -0,0 +1,9 @@
+package com.github.tonivade.purefun.concurrent;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+final class DefaultExecutor {
+
+  static final Executor EXECUTOR = Executors.newVirtualThreadPerTaskExecutor();
+}
