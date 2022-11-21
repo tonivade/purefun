6a0edd6ea83e018892ba0c163651ce1dc5840696
Antonio Muñoz <antoniogmc@gmail.com>
2022-11-21 13:30:08 +0100

    add missing copyright notice

--- core/src/main/java19/com/github/tonivade/purefun/concurrent/DefaultExecutor.java
+++ core/src/main/java19/com/github/tonivade/purefun/concurrent/DefaultExecutor.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2018-2022, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
 package com.github.tonivade.purefun.concurrent;
 
 import java.util.concurrent.Executor;
