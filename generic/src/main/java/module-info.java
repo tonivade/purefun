cc5540ac9b2e53304401e56a18d7caf4623092a7
Antonio Muñoz <antoniogmc@gmail.com>
2024-02-11 15:53:32 +0100

    java modules v2

---
+++ generic/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.github.tonivade.purefun.generic {
+  exports com.github.tonivade.purefun.generic;
+
+  requires com.github.tonivade.purefun;
+  requires com.github.tonivade.purefun.core;
+}
---
+++ instances/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.github.tonivade.purefun.instances {
+  exports com.github.tonivade.purefun.runtimes;
+  exports com.github.tonivade.purefun.instances;
+
+  requires com.github.tonivade.purefun;
+  requires com.github.tonivade.purefun.core;
+  requires com.github.tonivade.purefun.effect;
+  requires com.github.tonivade.purefun.free;
+  requires com.github.tonivade.purefun.monad;
+  requires com.github.tonivade.purefun.stream;
+  requires com.github.tonivade.purefun.transformer;
+  requires com.github.tonivade.purefun.typeclasses;
+  requires com.github.tonivade.purefun.control;
+}
