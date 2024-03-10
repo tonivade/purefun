c9e6092d0dc78149ef879ce14b3e2878b009080e
Antonio Muñoz <antoniogmc@gmail.com>
2024-03-10 01:05:52 +0100

    add annotation Initializer

---
+++ annotation/src/main/java/com/github/tonivade/purefun/Initializer.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2018-2024, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
+package com.github.tonivade.purefun;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Initializer {
+
+}
