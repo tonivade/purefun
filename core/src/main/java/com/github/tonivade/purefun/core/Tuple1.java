e97f8aae245a260951f2a071b7fba0aed77b2e7a
Antonio Muñoz <antoniogmc@gmail.com>
2024-04-21 16:56:24 +0200

    update email

--- core/src/main/java/com/github/tonivade/purefun/core/Tuple1.java
+++ core/src/main/java/com/github/tonivade/purefun/core/Tuple1.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2024, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Copyright (c) 2018-2024, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
  * Distributed under the terms of the MIT License
  */
 package com.github.tonivade.purefun.core;
