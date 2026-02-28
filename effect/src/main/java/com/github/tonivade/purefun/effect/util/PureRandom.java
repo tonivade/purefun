29a3b38c16fdab0af5ba5362d51f8027e5651a28
Antonio Muñoz <antoniogmc@gmail.com>
2026-02-28 14:20:43 +0100

    Update copyright year to 2026 in all relevant files

--- effect/src/main/java/com/github/tonivade/purefun/effect/util/PureRandom.java
+++ effect/src/main/java/com/github/tonivade/purefun/effect/util/PureRandom.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2025, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
+ * Copyright (c) 2018-2026, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
  * Distributed under the terms of the MIT License
  */
 package com.github.tonivade.purefun.effect.util;
